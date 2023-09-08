3dbdc4061039d2ba2f73179f022d89b4f08b6514
Lars Koopmans <larsk2009@live.nl>
2023-09-08 16:28:03 +0200

    Commit all pending changes

---
+++ RealShuffle/Globals.cs
@@ -0,0 +1,29 @@
+﻿using SpotifyAPI.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealShuffle
+{
+  public static class Globals
+  {
+    public static SpotifyClientConfig DefaultConfig = SpotifyClientConfig.CreateDefault();
+
+    private static Random rng = new Random();
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+      int n = list.Count;
+            Console.WriteLine(n);
+      while (n > 1)
+      {
+        n--;
+        int k = rng.Next(n + 1);
+        T value = list[k];
+        list[k] = list[n];
+        list[n] = value;
+      }
+    }
+  }
+}
